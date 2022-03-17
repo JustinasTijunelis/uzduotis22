202295e3ed758fdc8e657ced221b76c17b5e5fb6
verydemanding <58390763+verydemanding@users.noreply.github.com>
2022-03-17 20:09:24 +0200

    pirmauzduotis

---
+++ uzduotis22/uzduotis22/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace uzduotis22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 4 * 3.14 * 6371;
+            Console.WriteLine($"Rutulio pavirsiaus plotas: {x}");
+        }
+    }
+}
